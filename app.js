61bb1b92900d402b598497042a6acab18ba944c3
VarshaBlore <31268484+VarshaBlore@users.noreply.github.com>
2017-10-03 12:53:50 +0530

    updated in app.js
    File name updated to 'SnakesNLadders'

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express'); 
 var app = express();
 
-app.use(express.static("./views"));
+app.use(express.static("./SnakesNLadders"));
 app.listen(3000);
 module.exports = app;
